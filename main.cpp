f2611bc78e1fd436a90696660e4eec3ab61eb037
Nver Khachoyan <khachoyannver@gmail.com>
2022-12-29 16:05:49 -0800

    Case2 added for remove method, other improvements

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
+#include <string.h>
+
 #include <iostream>
-#include <string>
 
 // A dictionary Implementation
 
 struct Node {
-    std::string *word, *meaning;
+    std::string word, meaning;
     Node *left, *right;
 
-    Node(std::string *newWord, std::string *newMeaning) {
-        this->word = newWord;
-        this->meaning = newMeaning;
-        this->left = nullptr;
-        this->right = nullptr;
-    };
+    Node(std::string newWord, std::string newMeaning)
+        : word(newWord), meaning(newMeaning), left(nullptr), right(nullptr) {}
 };
 
 class Dictionary {
@@ -22,7 +19,7 @@ class Dictionary {
    public:
     Dictionary() { this->root = nullptr; };
 
-    void insert(std::string *word, std::string *meaning) {
+    void insert(std::string word, std::string meaning) {
         Node *newNode = new Node(word, meaning);
 
         if (!root) {
@@ -34,101 +31,126 @@ class Dictionary {
         Node *parent = nullptr;
 
         while (curr != nullptr) {
-            parent = curr;
-            if (*newNode->word < *curr->word) {
+            if (newNode->word.compare(curr->word) < 0) {
+                parent = curr;
                 curr = curr->left;
-            } else if (*newNode->word > *curr->word) {
+            } else if (newNode->word.compare(curr->word) > 0) {
+                parent = curr;
                 curr = curr->right;
             } else {
                 return;  // simply return if duplicate element is inserted
             }
         }
 
-        if (parent->left == nullptr) {
+        if (parent->word.compare(word) > 0) {
             parent->left = newNode;
         } else {
             parent->right = newNode;
         }
     };
 
-    void remove(std::string *word) {
-        // case 0, BST does not exist
-        if (!root) {
-            std::cout << "Empty dictionary.\n";
-            return;
-        }
-
-        // case 1, leaf node
-        Node *curr = this->root;
+    std::pair<Node *, Node *> search(std::string word) {
+        Node *curr = root;
         Node *parent = curr;
 
         while (curr != nullptr) {
-            if (*word < *curr->word) {
+            if (word.compare(curr->word) < 0) {
                 parent = curr;
                 curr = curr->left;
-            } else if (*word > *curr->word) {
+            } else if (word.compare(curr->word) > 0) {
                 parent = curr;
                 curr = curr->right;
             } else {
-                std::cout << "Found element.\n";
                 break;  // found element
             }
         }
 
-        if (curr == nullptr) {
-            std::cout << "Not found to be deleted.\n";
-            return;
-        }
-
-        if (*parent->left->word == *word) {
-            Node *tmp = parent->left;
-            parent->left = nullptr;
-            delete tmp;
-        } else {
-            Node *tmp = parent->right;
-            parent->right = nullptr;
-            delete tmp;
-        }
-
-        // case 1 and 2 will be implemented soon
+        return std::make_pair(curr, parent);
     };
 
-    void inOrderTraversal(Node *currNode) {
+    bool remove(std::string word) {
+        // case 0, BST does not exist
+        if (!root) {
+            return false;
+        }
+
+        // BST exists, we search for Node with key word
+        std::pair<Node *, Node *> found = search(word);
+        Node *foundNode = found.first;
+        Node *parentNode = found.second;
+
+        if (foundNode == nullptr) {
+            return false;
+        }
+
+        // case 1, leaf node
+        if (foundNode->left == nullptr && foundNode->right == nullptr) {
+            if (parentNode->left->word == word) {
+                Node *tmp = parentNode->left;
+                parentNode->left = nullptr;
+                delete tmp;
+            } else {
+                Node *tmp = parentNode->right;
+                parentNode->right = nullptr;
+                delete tmp;
+            }
+            return true;  // successfully deleted
+        }
+        // case 2, one child
+        else if (foundNode->left == nullptr || foundNode->right == nullptr) {
+            Node *temp = foundNode;
+            Node *foundNodeChild =
+                foundNode->left != nullptr ? foundNode->left : foundNode->right;
+
+            if (parentNode->left == foundNode) {
+                parentNode->left = foundNodeChild;
+            } else {
+                parentNode->right = foundNodeChild;
+            }
+
+            delete temp;
+            return true;
+        }
+        // case 3, two children
+        else {
+            std::cout << "Two children\n";
+        }
+
+        return false;
+    };
+
+    void inOrder(Node *currNode) {
         if (currNode == nullptr) {
             return;
         }
-        inOrderTraversal(currNode->left);
-        std::cout << *currNode->word << " : " << *currNode->meaning << "\n";
-        inOrderTraversal(currNode->right);
+        inOrder(currNode->left);
+        std::cout << currNode->word << " : " << currNode->meaning << "\n";
+        inOrder(currNode->right);
     };
 
-    Node *getRoot() { return this->root; };
+    Node *getRoot() { return root; };
 };
 
 int main() {
     Dictionary myDict;
 
-    std::string newWord = "Hellow";
-    std::string newMeaning = "Greetin";
-    myDict.insert(&newWord, &newMeaning);
+    myDict.insert("Hellow", "Greetin");
+    myDict.insert("Latte", "Coffee");
+    myDict.insert("Yellow", "Color");
+    myDict.insert("A", "Color");
+    myDict.insert("B", "Color");
+    myDict.insert("C", "Color");
+    myDict.insert("K", "Color");
+    myDict.insert("T", "Color");
+    myDict.insert("P", "Color");
 
-    std::string word1 = "Latte";
-    std::string meaning1 = "Coffee";
-    myDict.insert(&word1, &meaning1);
+    if (myDict.remove("B")) {
+        std::cout << "Removed.\n";
+    } else {
+        std::cout << "Not found.\n";
+    }
 
-    std::string word2 = "Yellow";
-    std::string meaning2 = "Color";
-    myDict.insert(&word2, &meaning2);
-
-    std::string word3 = "Yellow";
-    std::string meaning3 = "Color";
-    myDict.insert(&word3, &meaning3);
-
-    std::string tempWord = "Green";
-
-    myDict.remove(&word3);
-
-    myDict.inOrderTraversal(myDict.getRoot());
+    myDict.inOrder(myDict.getRoot());
 
     return 0;
 }
